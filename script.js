56738341524240913ac49e3e0427ad6a156bb829
ktaylor99 <132228170+ktaylor99@users.noreply.github.com>
2023-06-01 10:49:45 -0500

    Updated array, cards now truly random

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
-let firstCard = 11
-let secondCard = 10
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let sum = firstCard + secondCard
 let message = document.querySelector("#message")
 let cards = document.querySelector("#cards")
 let total = document.querySelector("#sum")
 let wins = document.querySelector("#wins")
 
+
 let allCards = [firstCard, secondCard]
 
 let hasBlackJack = false 
 let isAlive = false 
 
-let score = 1
+function getRandomCard(){
+    let cardRandom = Math.floor(Math.random() * 13) + 1
+    if (cardRandom > 10){
+        return 10
+    } else if (cardRandom === 1) {
+        return 11
+    } else {
+        return cardRandom
+    }
+    
+} 
 
 function startGame(){
     renderGame()
 }
 
 function renderGame(){
+    cards.textContent = "Cards: "
+        for (let c = 0; c < allCards.length; c++) {
+            cards.textContent += allCards[c] + " "
+        }
+
     total.textContent = "Sum: " + sum
-    wins.textContent = "Wins: " + score
-    cards.textContent = "Cards: " + allCards[0] + " - " + allCards[1]
     if (sum <= 20){
         message.textContent = "Do you want to draw another card?"
     } else if (sum === 21) {
         message.textContent = "You've got Blackjack!"
         hasBlackJack = true
-        score += 1
     } else {
         message.textContent = "You're out of the game!"
         isAlive = false
@@ -35,8 +48,11 @@ function renderGame(){
 
 function newCard(){
     message.textContent = "This literally doesn't work yet lol"
-    let thirdCard = 4
-    sum += thirdCard
+    let thirdCard = getRandomCard()
+    allCards.push(thirdCard)
+    sum = sum + thirdCard
     renderGame()
 }
+
+
 
